f8516e942e6658bad8582a09b5b4a5ce84f22d4c
Rider <38857907+Hexman7@users.noreply.github.com>
2023-09-29 16:39:53 +0200

    added armed peasant

--- module_troops.py
+++ module_troops.py
@@ -333,6 +333,10 @@ troops = [
   ["townsman","Townsman","Townsmen",tf_guarantee_boots|tf_guarantee_armor,no_scene,reserved,fac_commoners,
    [itm_cleaver,itm_knife,itm_club,itm_staff,itm_dagger,itm_stones,itm_leather_cap,itm_linen_tunic,itm_coarse_tunic,itm_leather_apron,itm_nomad_boots,itm_wrapping_boots],
    def_attrib|level(4),wp(60),knows_common,mercenary_face_1, mercenary_face_2],
+  ["armed_farmer","Armed Farmer","Armed Farmers",tf_guarantee_armor|tf_guarantee_boots,0,0,fac_commoners,
+   [itm_cleaver,itm_pitch_fork,itm_sickle,itm_club,itm_stones,itm_axe,itm_tab_shield_heater_a,
+    itm_leather_cap,itm_felt_hat,itm_felt_hat,itm_linen_tunic,itm_coarse_tunic,itm_nomad_boots,itm_wrapping_boots],
+   str_6|agi_4|int_4 | cha_4|level(4),wp(60),knows_common|knows_athletics_1,man_face_middle_1, man_face_old_2],
   ["watchman","Watchman","Watchmen",tf_guarantee_boots|tf_guarantee_armor|tf_guarantee_shield,no_scene,reserved,fac_commoners,
    [itm_bolts,itm_spiked_club,itm_fighting_pick,itm_sword_medieval_a,itm_boar_spear,itm_hunting_crossbow,itm_light_crossbow,itm_tab_shield_round_a,itm_tab_shield_round_b,
     itm_padded_cloth,itm_leather_jerkin,itm_leather_cap,itm_padded_coif,itm_footman_helmet,itm_nomad_boots,itm_wrapping_boots],
@@ -3852,6 +3856,7 @@ troops = [
 
 upgrade2(troops,"farmer", "watchman","mercenary_soldier")	## mod edited
 upgrade2(troops,"townsman","watchman","mercenary_soldier")	## mod edited
+upgrade2(troops,"armed_farmer","watchman","mercenary_soldier")	## mod edited
 upgrade2(troops,"watchman","caravan_guard","mercenary_crossbowman")
 upgrade2(troops,"caravan_guard","mercenary_swordsman","mercenary_horseman")
 upgrade(troops,"mercenary_swordsman","hired_blade")
